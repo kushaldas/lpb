942f11c37798afb8d65057a47481abee717843ca
Kushal Das <mail@kushaldas.in>
2016-02-15 21:03:05 +0530

    Adds the theme

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,10 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath('_themes'))
+html_theme_path = ['_themes']
+html_theme = 'kr'
+
 
 # -- General configuration ------------------------------------------------
 
